95a132dd8dbbd8a1256015cd871cdd2a74a1c0fa
Romer <romer@nostal.dev>
2022-11-23 12:19:00 +0100

    Lijsten omgezet (sorry van code formatting, gaat automatisch)

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,28 +1,29 @@
 const items = [
-	{ label: 'Home', hash: '#home' },
-	{ label: 'Afbeeldingen', hash: '#afbeeldingen' },
-	{ label: 'Knoppen', hash: '#knoppen' },
-	{ label: 'Voorbeeld', hash: '#voorbeeld' }
-]
+  { label: "Home", hash: "#home" },
+  { label: "Afbeeldingen", hash: "#afbeeldingen" },
+  { label: "Knoppen", hash: "#knoppen" },
+  { label: "Lijsten", hash: "#lijsten" },
+  { label: "Voorbeeld", hash: "#voorbeeld" },
+];
 
 function getNavigationList() {
-	const list = document.getElementById('main-nav-items')
-	items.forEach((item) => {
-		const ancor = document.createElement('a')
-		ancor.innerHTML = `${item.label}`
-		ancor.href = item.hash
-		ancor.onclick = toggleMenu
-		list.appendChild(ancor)
-	})
+  const list = document.getElementById("main-nav-items");
+  items.forEach((item) => {
+    const ancor = document.createElement("a");
+    ancor.innerHTML = `${item.label}`;
+    ancor.href = item.hash;
+    ancor.onclick = toggleMenu;
+    list.appendChild(ancor);
+  });
 }
 
 function toggleMenu() {
-	const list = document.getElementById('main-nav-items')
-	list.classList.toggle('visible')
+  const list = document.getElementById("main-nav-items");
+  list.classList.toggle("visible");
 
-	const item = document.getElementById('main-nav-item')
-	item.classList.toggle('visible')
+  const item = document.getElementById("main-nav-item");
+  item.classList.toggle("visible");
 }
 
-window.toggleMenu = toggleMenu
-getNavigationList()
+window.toggleMenu = toggleMenu;
+getNavigationList();
--- scripts/router.js
+++ scripts/router.js
@@ -1,30 +1,58 @@
 const routes = [
-	{ title: '404', alias: '404', hash: '#404', directory: './components/404' },
-	{ title: 'Home', alias: 'home', hash: '#home', directory: './components/home' },
-	{ title: 'Afbeeldingen', alias: 'afbeeldingen', hash: '#afbeeldingen', directory: './components/afbeeldingen' },
-	{ title: 'Knoppen', alias: 'knoppen', hash: '#knoppen', directory: './components/knoppen' },
-	{ title: 'Voorbeeld', alias: 'voorbeeld', hash: '#voorbeeld', directory: './components/voorbeeld' },
-]
+  { title: "404", alias: "404", hash: "#404", directory: "./components/404" },
+  {
+    title: "Home",
+    alias: "home",
+    hash: "#home",
+    directory: "./components/home",
+  },
+  {
+    title: "Afbeeldingen",
+    alias: "afbeeldingen",
+    hash: "#afbeeldingen",
+    directory: "./components/afbeeldingen",
+  },
+  {
+    title: "Knoppen",
+    alias: "knoppen",
+    hash: "#knoppen",
+    directory: "./components/knoppen",
+  },
+  {
+    title: "Lijsten",
+    alias: "lijsten",
+    hash: "#lijsten",
+    directory: "./components/lijsten",
+  },
+  {
+    title: "Voorbeeld",
+    alias: "voorbeeld",
+    hash: "#voorbeeld",
+    directory: "./components/voorbeeld",
+  },
+];
 
 const locationHandler = async () => {
-	window.location.hash = window.location.hash ? window.location.hash : '#home'
-	const hash = window.location.hash
-	let route = routes.find((item) => item.hash === hash)
-	if (!route) {
-		route = routes[0]
-		window.location.hash = route.hash
-	}
+  window.location.hash = window.location.hash ? window.location.hash : "#home";
+  const hash = window.location.hash;
+  let route = routes.find((item) => item.hash === hash);
+  if (!route) {
+    route = routes[0];
+    window.location.hash = route.hash;
+  }
 
-	const html = await fetch(`${route.directory}/${route.alias}.html`).then((data) => data.text())
-	document.getElementById('main-content').innerHTML = html
-	document.title = route.title
+  const html = await fetch(`${route.directory}/${route.alias}.html`).then(
+    (data) => data.text()
+  );
+  document.getElementById("main-content").innerHTML = html;
+  document.title = route.title;
 
-	const link = document.getElementById('component-styles')
-	link.href = `${route.directory}/${route.alias}.css`
+  const link = document.getElementById("component-styles");
+  link.href = `${route.directory}/${route.alias}.css`;
 
-	const script = document.getElementById('component-script')
-	script.src = `${route.directory}/${route.alias}.js`
-}
+  const script = document.getElementById("component-script");
+  script.src = `${route.directory}/${route.alias}.js`;
+};
 
-window.addEventListener('hashchange', locationHandler)
-locationHandler()
+window.addEventListener("hashchange", locationHandler);
+locationHandler();
