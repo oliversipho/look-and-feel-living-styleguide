81eff85c570b904feb6ee0c20ec1ad2b95744385
Luuk <luukbrauckmann@gmail.com>
2022-11-28 16:48:43 +0100

    Hoofdletters in menu Default layout aanpassing

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,20 +2,20 @@ const items = [
 	{ label: 'Home', hash: '#home', directory: './components/home' },
 	{ label: 'Voorbeeld', hash: '#voorbeeld', directory: './components/voorbeeld' },
 	{ label: 'Afbeeldingen', hash: '#afbeeldingen', directory: './components/afbeeldingen' },
-	{ label: 'animatie', alias: 'animatie', hash: '#animatie', directory: './components/animatie' },
-	{ label: 'berichtgeving', alias: 'berichtgeving', hash: '#berichtgeving', directory: './components/berichtgeving' },
-	{ label: 'blokken', alias: 'blokken', hash: '#blokken', directory: './components/blokken' },
-	{ label: 'formulieren', alias: 'formulieren', hash: '#formulieren', directory: './components/formulieren' },
-	{ label: 'globals', alias: 'globals', hash: '#globals', directory: './components/globals' },
-	{ label: 'headings', alias: 'headings', hash: '#headings', directory: './components/headings' },
-	{ label: 'iconen', alias: 'iconen', hash: '#iconen', directory: './components/iconen' },
-	{ label: 'interactieve-componenten', alias: 'interactieve-componenten', hash: '#interactieve-componenten', directory: './components/interactieve-componenten' },
-	{ label: 'kleuren', alias: 'kleuren', hash: '#kleuren', directory: './components/kleuren' },
+	{ label: 'Animatie', alias: 'animatie', hash: '#animatie', directory: './components/animatie' },
+	{ label: 'Berichtgeving', alias: 'berichtgeving', hash: '#berichtgeving', directory: './components/berichtgeving' },
+	{ label: 'Blokken', alias: 'blokken', hash: '#blokken', directory: './components/blokken' },
+	{ label: 'Formulieren', alias: 'formulieren', hash: '#formulieren', directory: './components/formulieren' },
+	{ label: 'Globals', alias: 'globals', hash: '#globals', directory: './components/globals' },
+	{ label: 'Headings', alias: 'headings', hash: '#headings', directory: './components/headings' },
+	{ label: 'Iconen', alias: 'iconen', hash: '#iconen', directory: './components/iconen' },
+	{ label: 'Interactieve-componenten', alias: 'interactieve-componenten', hash: '#interactieve-componenten', directory: './components/interactieve-componenten' },
+	{ label: 'Kleuren', alias: 'kleuren', hash: '#kleuren', directory: './components/kleuren' },
 	{ label: 'Knoppen', alias: 'knoppen', hash: '#knoppen', directory: './components/knoppen' },
-	{ label: 'lijsten', alias: 'lijsten', hash: '#lijsten', directory: './components/lijsten' },
-	{ label: 'media', alias: 'media', hash: '#media', directory: './components/media' },
-	{ label: 'navigatie', alias: 'navigatie', hash: '#navigatie', directory: './components/navigatie' },
-	{ label: 'reclame', alias: 'reclame', hash: '#reclame', directory: './components/reclame' }
+	{ label: 'Lijsten', alias: 'lijsten', hash: '#lijsten', directory: './components/lijsten' },
+	{ label: 'Media', alias: 'media', hash: '#media', directory: './components/media' },
+	{ label: 'Navigatie', alias: 'navigatie', hash: '#navigatie', directory: './components/navigatie' },
+	{ label: 'Reclame', alias: 'reclame', hash: '#reclame', directory: './components/reclame' }
 ]
 
 function getNavigationList() {
